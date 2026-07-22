a69aa956b5f3599f74fa5093b5f97da95e02f039
sulav-712 <baskotasulab@gmail.com>
2026-07-22 15:28:40 +0545

    Modified

--- auth/logout.php
+++ auth/logout.php
@@ -1,12 +1,9 @@
 <?php
 if  (session_status() === PHP_SESSION_NONE) {
   session_start();
-  echo 'run successfully';
 }
 session_unset();
 session_destroy();
-header("Location: /index.php");
+header("Location: ../index.php");
 exit;
-
-echo 'run successfully';
 ?>
